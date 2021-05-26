5f9b9a301f25710eb0259192198c619dddcf97b8
hp <hp@LAPTOP-GSDGFMCS>
2021-05-26 15:35:03 +0530

    commit up to fb,insta,whatsapp

--- core_java/src/main/java/com/Switchcase.java
+++ core_java/src/main/java/com/Switchcase.java
@@ -1,40 +1,28 @@
 package com;
 
+import java.util.Scanner;
+
 public class Switchcase {
 
 	public static void main(String[] args) {
-		int marks=55;
 		int mm=2;
+		int Square;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the value:");
+		int n=sc.nextInt();
 		
-		if(marks>60) {
-			//System.out.println("First Division");
-			mm=1;
-		}
-		else {
-			if(marks>=45  && marks<60) {
-				//System.out.println("Second Division");
-				mm=2;
-			}
-			else {
-				if(marks>=33  && marks<45) {
-					//System.out.println("Third Division");
-					mm=3;
-				}
-				else {
-					//System.out.println("Fail");
-					mm=4;
-				}
-			}
-		}
+		
 			
 		switch(mm) {
-		case 1 : System.out.println("First Division");
+		case 1 : Square=(n*n);
+		         System.out.println("Sqaure of  "+n+"  is " +Square);
 		break;
-		case 2 : System.out.println("Second Division");
-		break;
-		case 3 : System.out.println("Third Division");
-		break;
-		case 4 : System.out.println("Fail");
+		case 2 : for(float i=0;i<n;i++) 
+		          {
+			        if((i*i)==n) 
+			        	System.out.println("Sqaure root  of  "+n+"  is " +i);
+			        
+		          }
 		break;
 		default : System.out.println("invalid choice");
 

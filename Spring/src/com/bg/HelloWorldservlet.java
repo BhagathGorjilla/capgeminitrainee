976094e5d5fdb23012d1ac043c39bb1d13c8eee6
BhagathGorjilla <84562740+BhagathGorjilla@users.noreply.github.com>
2021-06-30 10:45:43 +0530

    Spring
    spring day 1

---
+++ Spring/src/com/bg/HelloWorldservlet.java
@@ -0,0 +1,76 @@
+package com.bg;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldservlet
+ */
+@WebServlet("/HelloWorldServlet")
+public class HelloWorldservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorldservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+        System.out.println("constructor");
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init ivoked");
+	}
+	
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destroy");
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("do get invoked");
+		PrintWriter out = response.getWriter() ;
+		out.println("<h1>Hello World from greet");
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		System.out.println("post invoked");
+		String name = request.getParameter("t1");
+		PrintWriter out=     response.getWriter() ;
+		out.println("<h1>Hello World from post method"+name);
+	}
+
+}
